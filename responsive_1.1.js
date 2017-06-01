a995a3126efe2fd53f95dc3f7f46c9ae1ba45c4c
Michał Kozłowski <michalandrzejkozlowski@gmail.com>
2017-06-01 13:01:07 +0200

    Added files from PC

---
+++ responsive_1.1.js
@@ -0,0 +1,35 @@
+var d = new Date();
+document.getElementById("data").innerHTML = d.getHours() + ":" + d.getMinutes()
++ " " + d.getDate() + "." + Number(d.getMonth() + 1) + "." + d.getFullYear();
+
+$(document).ready(function(){
+    $("header").on("click", function() {
+        $(this).css("color", "white");
+    });
+
+    $("img").on("mouseenter", function() {
+        if($(window).width() >= 768){
+            $(this).css({
+                "width": "100%",
+                "padding-bottom": "250px"
+            });
+            $(this).siblings().css("display", "none");
+            $(this).parentsUntil("body").css("background", "black");
+            $("header, nav, footer").css("visibility", "hidden");
+            $("aside").css("display", "none");
+      }
+    });
+
+    $("img").on("mouseleave", function(){
+        if($(window).width() >= 768){
+            $(this).css({
+                "width": "50%",
+                "padding": "2px"
+            });
+            $(this).siblings().css("display", "block");
+            $(this).parentsUntil("body").css("background", "initial");
+            $("header, nav, footer").css("visibility", "initial");
+            $("aside").css("display", "initial");
+        }
+    });
+});
